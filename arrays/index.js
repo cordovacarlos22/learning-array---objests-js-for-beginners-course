dd8ebf4db0808d5cb04afdf681bd19024ad7d3b3
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-10 10:22:07 -0700

    how to access a particular possitions in arrays

---
+++ arrays/index.js
@@ -0,0 +1,13 @@
+// array 
+let numbers = [2,5,6,4,10,24,40,7];
+// returns length of array 
+console.log("Array length",numbers.length) 
+// gets a particular number in this case 6 
+console.log("get a particular number",numbers[2])
+// gets last number of array in this case 7  
+console.log("get last number of array",numbers[numbers.length -1 ]) 
+// gets a particular number in this case  4
+console.log("get a particular number(use this one better)",numbers.at(3)) 
+// gets last number of array in this case 7 
+console.log("get last number of array", numbers.at(-1)) 
+
