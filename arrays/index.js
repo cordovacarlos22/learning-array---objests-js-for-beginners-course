9f6ce1ad132d7c1ed25ac3f806c3964a48b945da
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-10 10:35:55 -0700

    how to prints number in a increasement & decreasement order using for loop

--- arrays/index.js
+++ arrays/index.js
@@ -1,5 +1,7 @@
 // array 
 let numbers = [2,5,6,4,10,24,40,7];
+
+// -----=== how to access a particular positions in arrays ===----
 // returns length of array 
 console.log("Array length",numbers.length) 
 // gets a particular number in this case 6 
@@ -10,4 +12,19 @@ console.log("get last number of array",numbers[numbers.length -1 ])
 console.log("get a particular number(use this one better)",numbers.at(3)) 
 // gets last number of array in this case 7 
 console.log("get last number of array", numbers.at(-1)) 
+
+// -----=== how to linear search in arrays one by one   ===----
+
+// prints each number inside numbers array one by one in a  increasement
+ for (let i = 0; i < numbers.length; i++) {
+   console.log("number index increasement", numbers[i]);
+  
+ }
+
+// prints each number inside numbers array one by one decreasement 
+for (let i = numbers.length -1 ; i >= 0; i--) {
+  console.log("number index decreasement", numbers[i]);
+
+}
+
 
